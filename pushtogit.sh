714ef76adb4b2eea4c3878ca1ef2906b824965c0
nvdinh185 <nvdinh0905671805@gmail.com>
2018-12-20 13:58:32 +0700

    first commit

--- pushtogit.sh
+++ pushtogit.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "first commit"
-git remote add origin https://github.com/nvdinh185/ionic-dinh-app.git
+#git remote add origin https://github.com/nvdinh185/ionic-dinh-app.git
 git push -u origin master
